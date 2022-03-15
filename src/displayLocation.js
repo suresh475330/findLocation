9a0f654d6dae068b739768381e521bec3a4a28b8
suresh kumar <suresh475330@gmail.com>
2022-03-15 20:00:40 +0530

    first commit

---
+++ src/displayLocation.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import northpic from './images/north.jpg';
+import southpic from './images/south.jpg';
+import './Location.css'
+
+// config file - more then data irunthuthu na atha intha mathire object ta covert pannikalam
+let locationConfig = {
+    North : {
+        text : 'hi your corrent location is North region',
+        picture : northpic
+    },
+    South : {
+        text : 'hi your corrent location is South region',
+        picture : southpic
+    }
+}
+
+let displaylocation = (props)=>{
+  
+   let region = (props.latitude > 0) ? 'North' : 'South';
+   let {text,picture} = locationConfig[region]
+
+   // method 2
+    /*
+    if( props.latitude > 0){
+       return <div>hi, your regien is north</div>
+    }
+    else{
+        return <div>hi, your regien is south</div>
+    }
+   */
+  return (
+      <div className = {region}>
+       <img src={picture} alt='location picture'/>
+       <h1>{text}</h1>
+      </div>
+
+  )
+}
+
+
+export default displaylocation;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import DisplayLocation from './displayLocation';
+
+class App extends React.Component{
+  
+ constructor(props){
+   super(props)
+   this.state = {latitude:null,errorMessage:''}
+
+   window.navigator.geolocation.getCurrentPosition(
+    (position) => {this.setState({latitude :position.coords.latitude})},
+    (error) => {this.setState({errorMessage:error.message})}
+  );
+
+ }
+
+
+  render(){
+   
+   if(this.state.errorMessage){
+     return <div> Error :{this.state.errorMessage}</div>
+   }
+    else if(this.state.latitude){
+     return <div><DisplayLocation latitude = {this.state.latitude}/></div>
+   }
+   else{
+     return <div>waiting for user permisstion</div>
+   }
+  
+}
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+   <App />,
+  document.querySelector('#root')
+)
